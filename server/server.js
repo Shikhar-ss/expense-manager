1e75ada306c73c15f83b421365b7ff05bb09d0c7
Shikhar-ss <shikhar.sharma.ss.24@gmail.com>
2021-01-21 20:50:59 +0530

    added mongo credentials to environment variables

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var app = express();
 var mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config()
+const connectionString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}.imkcr.mongodb.net/expense-manager?retryWrites=true&w=majority`;
 
 app.set('view engine','html');
 app.set('views', path.join(__dirname,'../client'));
@@ -11,7 +14,7 @@ app.use(express.static(path.join(__dirname, '../client')));
 app.use(bodyParser.json({limit:'50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
 
-mongoose.connect('mongodb+srv://myself007:myself007@expense-manager.imkcr.mongodb.net/expense-manager?retryWrites=true&w=majority');
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use('/',router);
 
